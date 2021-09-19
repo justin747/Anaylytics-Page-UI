0d2b4b151d860ca7a9ae3157bf781b78cef4a893
Justin747 <justin747@gmail.com>
2021-09-19 09:53:14 -0700

    Initial Commit

---
+++ Anaylytics Page UI/Anaylytics_Page_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anaylytics_Page_UIApp.swift
+//  Anaylytics Page UI
+//
+//  Created by Justin747 on 9/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Anaylytics_Page_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Anaylytics Page UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Anaylytics Page UI
+//
+//  Created by Justin747 on 9/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
