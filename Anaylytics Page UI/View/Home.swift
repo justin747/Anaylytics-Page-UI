04a656a390f1a2a303a88993fa0b69f8e89eaa1b
Justin747 <justin747@gmail.com>
2021-09-30 16:51:52 -0700

    Added Card shortcuts
    Added Card shortcuts scrollable HStack for favorites

--- Anaylytics Page UI/View/Home.swift
+++ Anaylytics Page UI/View/Home.swift
@@ -80,7 +80,14 @@ struct Home: View {
                 
                 HStack(spacing: 20) {
                     
+                    CardView(image: "youtube", title: "YouTube", price: "$ 25", color: Color.red)
+                    CardView(image: "apple", title: "Apple", price: "$ 2600", color: Color.gray)
+                    CardView(image: "xbox", title: "XBox", price: "$ 2", color: Color.green)
+
+                    
+                    
                 }
+                .padding()
             }
             
         }
@@ -89,7 +96,7 @@ struct Home: View {
     }
     
     @ViewBuilder
-    func CardView(image: String, title: String, price: String, color: Color) {
+    func CardView(image: String, title: String, price: String, color: Color) -> some View {
         
         VStack(spacing: 15) {
             
@@ -103,8 +110,18 @@ struct Home: View {
                 .background(color, in: Circle())
             
             Text(title)
+                .font(.title3.bold())
+            
+            Text(price)
+                .fontWeight(.semibold)
+                .foregroundColor(.gray)
+            
         }
-        
+        .padding(.vertical)
+        .padding(.horizontal, 25)
+        .background(.white, in: RoundedRectangle(cornerRadius: 15))
+        .shadow(color: .black.opacity(0.05), radius: 5, x: 5, y: 5)
+        .shadow(color: .black.opacity(0.03), radius: 5, x: -5, y: -5)
     }
 }
 
